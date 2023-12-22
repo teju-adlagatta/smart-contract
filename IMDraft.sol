d1c04b7062a6bd16940c02a41c2dc7fa917ad14b
teju-adlagatta <132986735+teju-adlagatta@users.noreply.github.com>
2023-12-22 21:55:30 +0530

    Update IMDraft.sol

--- IMDraft.sol
+++ IMDraft.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol"
 
 contract CompanyFunds {
     
